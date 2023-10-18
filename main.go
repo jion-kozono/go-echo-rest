2c2317be2be6e576c09285d623b0ace87df03234
jion_kozono <zeon66190329@gmail.com>
2023-10-18 21:42:12 +0900

    add: task

--- main.go
+++ main.go
@@ -13,6 +13,10 @@ func main() {
 	userRepository := repository.NewUserRepository(db)
 	userUsecase := usecase.NewUserUseCase(userRepository)
 	userController := controller.NewUserController(userUsecase)
-	e := router.NewRouter(userController)
+	taskRepository := repository.NewTaskRepository(db)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository)
+	taskController := controller.NewTaskController(taskUsecase)
+
+	e := router.NewRouter(userController, taskController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- router/router.go
+++ router/router.go
@@ -2,13 +2,13 @@ package router
 
 import (
 	"go-rest-api/controller"
+	"os"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
-func NewRouter(uc controller.IUserController,
-	// tc controller.ITaskController
-	) *echo.Echo {
+func NewRouter(uc controller.IUserController,tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
 	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 	// 	AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
@@ -29,15 +29,15 @@ func NewRouter(uc controller.IUserController,
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
 	// e.GET("/csrf", uc.CsrfToken)
-	// t := e.Group("/tasks")
-	// t.Use(echojwt.WithConfig(echojwt.Config{
-	// 	SigningKey:  []byte(os.Getenv("SECRET")),
-	// 	TokenLookup: "cookie:token",
-	// }))
-	// t.GET("", tc.GetAllTasks)
-	// t.GET("/:taskId", tc.GetTaskById)
-	// t.POST("", tc.CreateTask)
-	// t.PUT("/:taskId", tc.UpdateTask)
-	// t.DELETE("/:taskId", tc.DeleteTask)
+	t := e.Group("/tasks")
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token",
+	}))
+	t.GET("", tc.GetAllTasks)
+	t.GET("/:taskId", tc.GetTaskById)
+	t.POST("", tc.CreateTask)
+	t.PUT("/:taskId", tc.UpdateTask)
+	t.DELETE("/:taskId", tc.DeleteTask)
 	return e
 }
